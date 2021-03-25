9589d61291a0a125de26fd2f6266a74bf5957aba
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-25 09:34:57 +0530

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessa
 from youtubesearchpython import VideosSearch
 
 
-@tgbot.on(events.InlineQuery(pattern=r"youtube (.*)"))
+@Client.on(events.InlineQuery(pattern=r"youtube (.*)"))
 async def inline(client: Client, query: InlineQuery):
     answers = []
     search_query = query.query.lower().strip().rstrip()
